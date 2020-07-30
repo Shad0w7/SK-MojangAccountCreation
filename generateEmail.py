3b57831c5133b7cc38be0e20f4ba55907ecfa15e
Shad0w7 <ayush.nayak@gmail.com>
2020-07-29 21:56:39 -0700

    Added lots of new stuff

---
+++ generateEmail.py
@@ -0,0 +1,6 @@
+# generateEmail.py
+
+# Copyright (C) Ayush Nayak, Daniel Deng - All Rights Reserved
+# Unauthorized copying of this file, via any medium is strictly prohibited
+# Proprietary and confidential
+# Written by Ayush Nayak <shad0wasdfseven@gmail.com>, August 2020
