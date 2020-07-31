fdb0de3606d3863b2aa9e773c360cec817c28883
Shad0w7 <ayush.nayak@gmail.com>
2020-07-31 16:52:01 -0700

    NO FULL AUTO IN BUILDINGS

--- MojangAccountCreator.py
+++ MojangAccountCreator.py
@@ -121,7 +121,6 @@ driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () =>
 driver.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
 
 if Verbose:
-    
     print('Session User Agent: {}'.format(driver.execute_script("return navigator.userAgent;")))
 
 # Open the second window
@@ -282,8 +281,10 @@ f.close()
 
 if Verbose:
     print('Appended Text!')
+
 # Ready to Quit !
 
-# input('Program Finished! \nPress Enter to Quit. . . ') 
-print('Quitting. . .')
+# input('Program Finished! \nPress Enter to Quit. . . ') # Uncomment if you want control over what happens after a successful account creation
+if Verbose:
+    print('Quitting. . .')
 driver.quit()
